dd8882971055a1022317ed26e34ae5d18a6797ee
KnightLeo <knightleo602@gmail.com>
2024-09-29 20:15:08 -0300

    shiba

---
+++ assets/shiba/shiba.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func _init() -> void:
+	$AnimationPlayer.play("rabinho")
---
+++ scripts/shiba.gd
@@ -0,0 +1,18 @@
+extends CharacterBody3D
+
+@export var player: Node3D
+
+@export var speed: float = 10.
+@export var acceleration: float = 5.
+@export var max_player_distance: float = 15.
+
+func _physics_process(delta: float) -> void:
+	var player_distance = position.distance_to(player.position)
+	var direction = (player.position - position).normalized()
+	$Pivot.basis = lerp($Pivot.basis, Basis.looking_at(direction), delta * acceleration)
+	if player_distance > max_player_distance:
+		velocity.x = lerp(velocity.x, direction.x * speed, acceleration * delta)
+		velocity.z = lerp(velocity.z, direction.z * speed, acceleration * delta)
+	else:
+		velocity = lerp(velocity, Vector3.ZERO, acceleration * delta)
+	move_and_slide()
