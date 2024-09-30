84dc2d3717a20a784cb99891fc840471796c10d2
KnightLeo <knightleo602@gmail.com>
2024-09-30 18:27:46 -0300

    tecla para fechar o jogo

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,5 @@
 extends Node
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Exit Game"):
+		get_tree().quit()
